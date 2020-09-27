438ab1ceb7b4d03f8e8adde930d17afba77b5170
Masaki Kobayashi <bayashi.cl@gmail.com>
2020-09-27 12:50:51 +0900

    comポート自動選択

--- serialHandler.py
+++ serialHandler.py
@@ -2,17 +2,19 @@ import re
 import time
 
 import serial
+from serial.tools import list_ports
 
 
 class SerialHandler():
     """シリアル通信ハンドラ
 
     シリアル通信から数値のリストへ変換するまでを行う
+    COMポートは指定したほうが無難
 
     Args:
-        port(str, optional): ポートを選択 初期値COM3
-        baudrate(int, optional): ボーレートを設定 初期値115200
-        timeout(int, optional): タイムアウトを設定 初期値1
+        port(str, optional): ポートを選択 初期値 自動選択
+        baudrate(int, optional): ボーレートを設定 初期値 115200
+        timeout(int, optional): タイムアウトを設定 初期値 1
 
     Attitudes:
         data(list[float]): 整形されたデータが収納される。更新は update() で
@@ -20,35 +22,39 @@ class SerialHandler():
         ser(serial.Serial): pyserial
     """
 
-    def __init__(self, port="COM3", baudrate=115200, timeout=1):
+    def __init__(self, port=None, baudrate=115200, timeout=1):
         self.data = []
         self.re_pattern = r'\n.+\r'
+
+        if port is None:
+            ports = list_ports.comports()
+            if ports == []:
+                print("cannot find comport")
+            port = ports[0].device
+        
         self.ser = serial.Serial(port, baudrate, timeout=timeout)
         time.sleep(3)
-        self.ser.flushInput()
-        print("init end")
+        print("serial init end")
 
     def __del__(self):
         self.ser.close()
 
     def update(self):
         """インスタンス変数 data を更新
-        
+
         returns:
             bool: True なら成功 False なら失敗 data の変化なし
         """
         # 元データ -> デコード -> /nと/rに囲まれている部分を検索 -> 最新のものを数値のリストに
         data_raw = self.ser.read_all().decode()
         data_list = re.findall(self.re_pattern, data_raw)
-        
+
         if data_list == []:
             return False
-        else:
-            pass
-        
+
         data_str = data_list[-1].strip().split(',')
         self.data = [float(s) for s in data_str]
-        
+
         return True
 
 
